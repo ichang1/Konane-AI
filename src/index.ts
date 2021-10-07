a505564a6d1edc8efe06b7540bece02893ca7017
ichang1 <isaac.chang717@gmail.com>
2021-10-07 02:10:51 -0400

    set up ts

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// import PriorityQueue from "./utils/PriorityQueue";
+// import Konane from "./utils/Konane";
+
+// const pq = new PriorityQueue((el: number) => el);
+// console.log(pq.isEmpty());
+// pq.push(100);
+// pq.push(90);
+// pq.push(80);
+// pq.push(60);
+// pq.pop();
+// console.log(pq);
+
+// const game = new Konane(8);
+// console.log(game.board);
+// console.log(game.blackPositions.values());
+// console.log(game.whitePositions.values());
