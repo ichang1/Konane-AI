b0785d2310bb210366ca0520201ceae9202e8911
ichang1 <isaac.chang717@gmail.com>
2021-10-23 01:18:56 -0400

    left and right side bar

--- konane-ai/components/SideBar/SideBar.tsx
+++ konane-ai/components/SideBar/SideBar.tsx
@@ -1,30 +1,78 @@
 import React from "react";
+import { konaneDifficulties } from "../../konane/KonaneUtils";
 import styles from "../../styles/components/SideBar/SideBar.module.scss";
 interface SideBarProps {
+  navigation: boolean;
   className?: string;
-  children: React.ReactNode;
+  children?: React.ReactNode;
+  left?: boolean;
+  right?: boolean;
 }
 
-const SideBar: React.FC<SideBarProps> = ({ className, children }) => {
-  return (
-    <div
-      className={`${styles["sidebar"]} ${className ? className : ""}`.trim()}
-    >
-      <div>
-        <button className={styles["sidebar-button"]}>
-          <a href={"/"} className={styles["sidebar-button-home-link"]}>
-            Home
-          </a>
-        </button>
-        <button className={styles["sidebar-button"]}>
-          <a href={"/"} className={styles["sidebar-button-about-link"]}>
-            About
-          </a>
-        </button>
-      </div>
-      {children}
+const sidebarContent = (
+  <>
+    <div>
+      <button className={styles["sidebar-button"]}>
+        <a href={"/"} className={styles["sidebar-button-home-link"]}>
+          Home
+        </a>
+      </button>
+      <button className={styles["sidebar-button"]}>
+        <a href={"/"} className={styles["sidebar-button-about-link"]}>
+          About
+        </a>
+      </button>
     </div>
-  );
+    <div className={styles["sidebar-difficulty-buttons-container"]}>
+      {Object.keys(konaneDifficulties).map((diff, idx) => (
+        <button
+          data-level={idx}
+          className={styles["sidebar-difficulty-button"]}
+          key={idx}
+        >
+          <a
+            key={idx}
+            href={`/${diff}`}
+            className={styles["sidebar-difficulty-button-link"]}
+          >
+            {diff.toUpperCase()}
+          </a>
+        </button>
+      ))}
+    </div>
+  </>
+);
+
+const SideBar: React.FC<SideBarProps> = ({
+  className,
+  children,
+  left,
+  right,
+  navigation,
+}) => {
+  if (left || (!left && !right)) {
+    return (
+      <div
+        className={`${styles["left-sidebar"]} ${
+          className ? className : ""
+        }`.trim()}
+      >
+        {navigation && sidebarContent}
+        {children}
+      </div>
+    );
+  } else {
+    return (
+      <div
+        className={`${styles["right-sidebar"]} ${
+          className ? className : ""
+        }`.trim()}
+      >
+        {navigation && sidebarContent}
+        {children}
+      </div>
+    );
+  }
 };
 
 export default SideBar;
--- konane-ai/pages/_app.tsx
+++ konane-ai/pages/_app.tsx
@@ -1,7 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import React from "react";
+import SideBar from "../components/SideBar/SideBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <SideBar navigation={true} left={true} />
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
